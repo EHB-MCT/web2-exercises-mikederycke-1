b695bd7b5211e427eb716a985efe44e9c89386f9
mikederycke <mike.derycke@ehb.be>
2021-10-11 11:27:51 +0200

    Session 1 Complete with Bonus

--- S1/Bonus/script.js
+++ S1/Bonus/script.js
@@ -14,8 +14,30 @@ const dishes = [{
     price: 10
 }]
 
+
+let orders = [];
+initializeOrders();
+
+
 window.onload = function(){
     console.log('loaded');
+
+    //Dynamically build the radiobuttons
+    dishes.forEach(dish => {
+        //Create a DIV 
+        let radio = document.createElement('div');
+        //Add code to the DIV
+        radio.innerHTML = `
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="dish" id="dish${dish.id}" 
+                value="${dish.id}" ${dish.id == 1 ? 'checked' : ''}>
+                <label class="form-check-label" for="exampleRadios1">
+                    ${dish.name}
+                </label>
+              </div>`;
+        // Add each DIV to the page
+        document.getElementById('orderOptions').appendChild(radio);
+    });
 
     // Add event listener to submit button of the FORM
     document.getElementById('form')
@@ -38,12 +60,36 @@ window.onload = function(){
             name,email,dish
         }
 
+        //Save the order (array and localstorage)
+        saveOrder(orderdetails);
+
         //Print on the screen
         document.getElementById('messages').innerHTML = printOrder(orderdetails);
+    
+
+    
     });
 }
 
 function printOrder(orderdetails){
     return `<p>The order for the customer ${orderdetails.name} is the following:  
         ${orderdetails.dish.name}. The customer may be notified by email: ${orderdetails.email}</p>`;
+}
+
+function saveOrder(order){
+    //Save the order to the array
+    orders.push(order);
+
+    //Save to localstorage
+    localStorage.setItem('orders', JSON.stringify(orders));
+}
+
+function initializeOrders(){
+    let storage = localStorage.getItem('orders');
+    if(storage !== null){
+        orders = JSON.parse(storage);
+    }else{
+        //Store the empty array
+        localStorage.setItem('orders', JSON.stringify(orders));
+    }
 }
