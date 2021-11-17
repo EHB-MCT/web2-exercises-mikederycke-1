fbdf6e13fc15b39238c2abdd770a2877f2cbee69
mikederycke <mike.derycke@ehb.be>
2021-11-17 12:04:07 +0100

    Finished S3/S4 code

--- S3/script.js
+++ S3/script.js
@@ -1,15 +1,85 @@
-let baseurl = 'http://www.omdbapi.com/?apikey=10531ad7&';
+import url from './config.js';
+let counter = 0;
+let currentMovie = {};
 
 window.onload = function(){
-    document.getElementById('searchform').addEventListener('submit', event => {
-        event.preventDefault();
-        //Build the url
-        let url = baseurl + `t=${document.getElementById('input').value}`;
-        // Get the data
-        getData(url).then(movie => {
-            addMovie(movie);
+    updateCounter();
+    //get the value from inputfield on submit
+    document.getElementById('inputTitle').addEventListener('keyup', event => {
+      
+        //retrieve value from inputfield
+        let value = document.getElementById('inputTitle').value;
+        
+        //build the url
+        let finalUrl = `${url}&s=${value}`;
+
+        //fetch the data
+        getData(finalUrl).then(result => {
+            updateList(result);
+            // addMovie(result);
         });
+        
     });
+
+    //Add the add movie button event listener
+    document.getElementById('addMovie').addEventListener('click', event =>{
+        if(currentMovie){
+            counter += parseInt(currentMovie.Runtime);
+            updateCounter();
+        }
+        
+    });
+
+    Array.from(document.getElementsByClassName('list-group-item')).forEach(element => {
+        element.addEventListener('click', selectMovie);
+    });
+}
+
+function selectMovie(event){
+    let str = event.target.text;
+    let year = str.split('- Year: ')[1].trim();
+    let title = str.split('- Year: ')[0].trim();
+    
+    //Get movie from api
+    //build the url
+    let finalUrl = `${url}&t=${title}&y=${year}`;
+
+    //fetch the data
+    getData(finalUrl).then(result => {
+        addMovie(result);
+        // hide resultlist
+        document.getElementById('resultlist').setAttribute('hidden', true);
+    });
+
+}
+
+function updateList(list){
+    console.log(list);
+    if(list.Error){
+        document.getElementById(`error`).innerText = 'No movies found! Keep trying';
+    }
+    if(list.Search){
+        let count = 1;
+        list.Search.forEach(movie => {
+            let string = `${movie.Title} - Year: ${movie.Year}`;
+            document.getElementById(`result${count}`).innerText = string;
+            count++;
+        });
+        document.getElementById('resultlist').removeAttribute('hidden');
+        document.getElementById('error').setAttribute('hidden', true);
+    }
+    
+    
+}
+
+function addMovie(m){
+    currentMovie = m;
+    document.getElementById('poster').src = m.Poster;
+    document.getElementById('cardtitle').innerText = m.Title;
+    document.getElementById('carddesc').innerText = m.Plot;
+    document.getElementById('carddirector').innerText = `Director: ${m.Director}`;
+    document.getElementById('cardruntime').innerText = `Year: ${m.Year} - Runtime: ${m.Runtime}`;
+    document.getElementById('moviecard').removeAttribute('hidden');
 }
 
 async function getData(url){
@@ -17,9 +87,17 @@ async function getData(url){
     return await response.json();
 }
 
-function addMovie(m){
-    document.getElementById('poster').src = m.Poster;
-    document.getElementById('title').innerText = m.Title;
-    document.getElementById('desc').innerText = m.Plot;
-    document.getElementById('runtime').innerText = m.Runtime;
+function updateCounter(){
+    if(counter === 0){
+        if(localStorage.getItem('movieCounter')){
+            counter = parseInt(localStorage.getItem('movieCounter'));
+        }
+    }
+    document.getElementById('counter').innerText = counter;
+
+    //save latest state to local storage
+    localStorage.setItem('movieCounter', counter);
 }
+
+
+
